b810682b77a123e5de877bf1fc6c96a78c907a3f
anhntt08 <32409695+anhntt08@users.noreply.github.com>
2018-11-06 12:14:27 +0700

    delete

--- app/src/main/java/safeobject/guardapplication/MyAppGlideModule.java
+++
@@ -1,8 +0,0 @@
-package safeobject.guardapplication;
-
-import com.bumptech.glide.module.AppGlideModule;
-
-public class MyAppGlideModule extends AppGlideModule {
-    private void test(){
-    }
-}
